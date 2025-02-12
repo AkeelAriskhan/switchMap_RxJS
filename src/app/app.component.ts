72b6e2c63b680e10092ff320187b2dddfe55bc2c
AkeelAriskhan UCA <akeel@unicornconnectedapps.com>
2025-02-12 09:22:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { fromEvent } from 'rxjs';
+import { debounceTime, map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Anguartesting';
+  ngOnInit() {
+    const searchBox = document.getElementById('searchBox');
+
+    fromEvent(searchBox!, 'input').pipe(
+      debounceTime(2000), // Wait for user to stop typing for 500ms
+      map((event: any) => event.target.value), // Extract input value
+      switchMap(value => {
+        console.log('User Input:', value);
+        return value; // Not calling API here, just logging
+      })
+    ).subscribe( );
+  }
 }
